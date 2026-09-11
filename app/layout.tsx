4f80080cef926da3ac0d32bbc9eca7658ed9a2ef
carovi85 <carmenromerovivero@hotmail.com>
2026-09-11 22:16:15 +0200

    fix: bloquear traduccion automatica en movil

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="es"
+      translate="no"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased notranslate`}
     >
+      <head>
+        <meta name="google" content="notranslate" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
